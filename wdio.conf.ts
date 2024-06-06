f2f344f8b68d074f7e0a57f81102da484750ef7d
Krishna Kumar <profile.kumarkrish@gmail.com>
2024-06-06 22:31:31 +0530

    updated edge browser details

--- wdio.conf.ts
+++ wdio.conf.ts
@@ -60,9 +60,9 @@ export const config: Options.Testrunner = {
     //
     capabilities: [{
         browserName: 'chrome'
-    } /*{
+    }, {
        browserName: 'MicrosoftEdge'
-    }*/
+    }
 ],
 
     //
